1c3ae4f8529b306dd3b70adefd35b8829a9f9bd3
neoma287 <79696395+neoma287@users.noreply.github.com>
2022-04-18 21:25:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+
+  video = createCapture(VIDEO);
+  video.size(800,400);
+
+  video.parent('game_console');
+
+  poseNet = ml5.poseNet(modelLoaded);
+  pose.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('MODEL LOADED');
+}
+
+function draw() {
+	game()
+}
+
+
+function gotPoses(results){
+  if(results.length>0)
+    {
+      console.log(results);
+      noseX=results[0].pose.nose.x;
+      noseY=results[0].pose.nose.y;
+      
+}
+}
+
+
+
